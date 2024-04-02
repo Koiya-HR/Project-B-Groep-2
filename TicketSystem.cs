eb4492edcd7be8a8ebba89541c5af1fb03090bbd
RafCamora123 <159793458+RafCamora123@users.noreply.github.com>
2024-04-02 10:29:10 +0200

    Create TicketSystem.cs

---
+++ TicketSystem.cs
@@ -0,0 +1,47 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TicketSystem
+{
+    class Ticket
+    {
+        public string Film { get; set; }
+        public string Genre { get; set; }
+        public int Stoelen { get; set; }
+        public double Prijs { get; set; }
+    }
+
+    class TicketController
+    {
+        private List<Ticket> tickets = new List<Ticket>();
+        
+        public TicketController(string jsonFilePath)
+        {
+            string jsonData = File.ReadAllText(jsonFilePath);
+            tickets = JsonConvert.DeserializeObject<List<Ticket>>(jsonData);
+        }
+
+        public void ShowTicketInfo()
+        {
+            Console.WriteLine("Ticket informatie:");
+            foreach (Ticket ticket in tickets)
+            {
+                Console.WriteLine($"Film: {ticket.Film}");
+                Console.WriteLine($"Genre: {ticket.Genre}");
+                Console.WriteLine($"Stoelen: {ticket.Stoelen}");
+                Console.WriteLine($"Prijs: {ticket.Prijs:C}");
+                Console.WriteLine();
+            }
+        }
+
+        public void ShowTotalAmount()
+        {
+            foreach (Ticket ticket in tickets)
+            {
+                Console.WriteLine($"Totaalbedrag voor alle tickets: {ticket.Prijs:C}");
+            }
+        }
+    }
+}
