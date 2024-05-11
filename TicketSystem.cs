9ae9357531345093488e0d1681bd3b996feb32a4
IvarHendriks1 <160335970+IvarHendriks1@users.noreply.github.com>
2024-05-11 15:41:32 +0200

    Create TicketSystem.cs

---
+++ TicketSystem.cs
@@ -0,0 +1,46 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class TicketController
+{
+    private List<Ticket> tickets = new List<Ticket>();
+
+    public TicketController(string jsonFilePath)
+    {
+        string jsonData = File.ReadAllText(jsonFilePath);
+        tickets = JsonConvert.DeserializeObject<List<Ticket>>(jsonData)!;
+    }
+
+
+    public void ShowTicketInfo()
+    {
+        Console.WriteLine("Ticket informatie:");
+        foreach (Ticket ticket in tickets)
+        {
+            Console.WriteLine($"Film ID: {ticket.FilmID}");
+            Console.WriteLine($"stoel: {ticket.StoelNaam}");
+            Console.WriteLine($"row in array: {ticket.StoelRow}");
+            Console.WriteLine($"col in array: {ticket.StoelCol}");
+            Console.WriteLine($"event id: {ticket.EventID}");
+            Console.WriteLine($"prijs: {ticket.Prijs}");
+            Console.WriteLine();
+        }
+        if (tickets.Count == 0)
+        {
+            Console.WriteLine("je hebt geen tickets");
+        }
+    }
+
+    public void ShowTotalAmount()
+    {
+        double prijs = 0;
+        foreach (Ticket ticket in tickets)
+        {
+            prijs += ticket.Prijs;
+        }
+        Console.WriteLine($"Totaalbedrag voor alle tickets: {prijs:C}");
+    }
+}
